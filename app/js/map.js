617301f8ae3b02426e99ee84c392f24876a7add9
Magnum888 <K.O.M.888@i.ua>
2018-05-14 22:45:46 +0300

    geolocation main map

--- app/js/map.js
+++ app/js/map.js
@@ -1,11 +1,25 @@
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
+  var pos = {};
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+    function showPosition(position){
+      pos.lat = position.coords.latitude;
+      pos.lng = position.coords.longitude;
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: pos
+      });
+      var marker = new google.maps.Marker({
+        position: pos,
+        map: map
+      });
+    }
+  }else{
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
+      zoom: 10,
+      center: {lat: -34.397, lng: 150.644}
     });
   }
+  
+    
+}
