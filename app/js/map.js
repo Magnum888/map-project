b08b46a75c15d6b15fa5019a531306108cc9d6b0
Magnum888 <K.O.M.888@i.ua>
2018-05-15 01:37:27 +0300

    add geolocation user

--- app/js/map.js
+++ app/js/map.js
@@ -1,25 +1,62 @@
 function initMap() {
   var pos = {};
+  var mapInclude = document.getElementById('map');
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-    function showPosition(position){
-      pos.lat = position.coords.latitude;
-      pos.lng = position.coords.longitude;
-      var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
-        center: pos
-      });
-      var marker = new google.maps.Marker({
-        position: pos,
-        map: map
-      });
-    }
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
   }else{
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 10,
-      center: {lat: -34.397, lng: 150.644}
+      center: {lat: 34.052235, lng: -118.243683}
     });
   }
-  
+  function showPosition(position){
+    pos.lat = position.coords.latitude;
+    pos.lng = position.coords.longitude;
+    var options = {
+      zoom: 10,
+      center: pos
+    }
+    var map = new google.maps.Map(document.getElementById('map'), options);
     
-}
+    addMarker({
+      coordinates: pos,
+      image: '../img/map-marker.png',
+      info: '<div>You are here!</div>'
+    });
+
+    function addMarker(properties){
+      var marker = new google.maps.Marker({
+        position: properties.coordinates,
+        map: map,
+        icon: properties.image
+      });
+      if(properties.image){
+        marker.setIcon(properties.image);
+      };
+      if(properties.info){
+        var InfoWindow = new google.maps.InfoWindow({
+          content: properties.info
+        });
+        marker.addListener('click', function(){
+          InfoWindow.open(map, marker);
+        });
+      };
+    };
+  };
+  function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            mapInclude.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            mapInclude.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            mapInclude.innerHTML = "The request to get user location timed out."
+            break;
+            break;
+       case error.UNKNOWN_ERROR:
+            mapInclude.innerHTML = "An unknown error occurred."
+     };
+  };
+};
