7b4dc6fafd04daaf6806452af6a35d3cff28419d
Magnum888 <K.O.M.888@i.ua>
2018-05-15 18:26:58 +0300

    add location to farm

--- app/js/common.js
+++ app/js/common.js
@@ -4,17 +4,27 @@ $(function() {
 		var button = $(event.relatedTarget)
 		// console.log(button)
 		var namefarm = button.data('namefarm')
+		var lat = button.data('lat')
+		var lng = button.data('lng')
+		console.log(lat, lng)
 		var modal = $(this)
 		modal.find('.modal-title').text('Location: ' + namefarm)
-		var uluru = {lat: -25.363, lng: 131.044};
+		var uluru = {lat: +lat, lng: +lng};
         var map = new google.maps.Map(document.getElementById('mapsModal'), {
-          zoom: 13,
+          zoom: 18,
           center: uluru
         });
         var marker = new google.maps.Marker({
           position: uluru,
-          map: map
-        });
-	  })
+		  map: map,
+		  icon: '../img/map-marker.png'
+		});
+		var infoWindow = new google.maps.InfoWindow({
+			content: "<span>" + namefarm + "</span>"
+		});
+		marker.addListener('click', function(){
+			infoWindow.open(map, marker)
+		});
+	  });
 
 });
