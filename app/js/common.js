fc40bfee575845778549ca36f0ca82d5c1842128
Magnum888 <K.O.M.888@i.ua>
2018-05-17 18:43:00 +0300

    add markers on map

--- app/js/common.js
+++ app/js/common.js
@@ -2,13 +2,13 @@ $(function() {
 
 	$('#mapModal').on('show.bs.modal', function (event) {
 		var button = $(event.relatedTarget)
-		// console.log(button)
 		var namefarm = button.data('namefarm')
+		var productfarm = button.offsetParent().children().find(".farm__type--product").text()
+		var addressfarm = button.offsetParent().children().find(".farm__type--address").text()
 		var lat = button.data('lat')
 		var lng = button.data('lng')
-		console.log(lat, lng)
 		var modal = $(this)
-		modal.find('.modal-title').text('Location: ' + namefarm)
+		modal.find('.modal-title').text(namefarm)
 		var uluru = {lat: +lat, lng: +lng};
         var map = new google.maps.Map(document.getElementById('mapsModal'), {
           zoom: 18,
@@ -20,7 +20,7 @@ $(function() {
 		  icon: '../img/map-marker.png'
 		});
 		var infoWindow = new google.maps.InfoWindow({
-			content: "<span>" + namefarm + "</span>"
+			content: "<div class='marker--title'>" + namefarm + "</div>" + "<hr class='marker--hr'>" + "<div>Product: " + productfarm + "</div>" + "<hr class='marker--hr'>" + "<div> Address: " + addressfarm + "</div>" + "<hr class='marker--hr'>" + "<a href=''>show more</a>"
 		});
 		marker.addListener('click', function(){
 			infoWindow.open(map, marker)
--- app/js/map.js
+++ app/js/map.js
@@ -17,13 +17,57 @@ function initMap() {
       center: pos
     }
     var map = new google.maps.Map(document.getElementById('map'), options);
+    var markers = [
+      {
+        coordinates: pos,
+        image: '../img/map-marker.png',
+        info: '<div>You are here!</div>'
+      },
+      {
+        coordinates: {lat: 43.917775, lng: -78.587485},
+        image: '../img/farm-2.png',
+        info: '<div class="marker--title">"R" FARM </div>'+'<hr class="marker--hr">'+
+              '<div class="marker--product">Product: eggs</div>'+'<hr class="marker--hr">'+
+              '<div class="marker--address">Address: Newcastle, ON L1B1L9</div>'+'<hr class="marker--hr">'+
+              '<a href="#" class="marker--link">Show more</a>'
+      },
+      {
+        coordinates: {lat: 52.123013, lng: -106.755475},
+        image: '../img/farm-2.png',
+        info: '<div class="marker--title">"Saskatoon" FARM</div>'+'<hr class="marker--hr">'+
+              '<div class="marker--product">Product: beef</div>'+'<hr class="marker--hr">'+
+              '<div class="marker--address">Address: Saskatoon, SK S7M5G8</div>'+'<hr class="marker--hr">'+
+              '<a href="#" class="marker--link">Show more</a>'
+      },
+      {
+        coordinates: {lat: 45.66116015111812, lng: -64.39642475776519},
+        image: '../img/farm-2.png',
+        info: '<div class="marker--title">"Herbert" FARM </div>'+'<hr class="marker--hr">'+
+              '<div class="marker--product">Product: pigs</div>'+'<hr class="marker--hr">'+
+              '<div class="marker--address">Address: River Herbert East, NS B0L1H0</div>'+'<hr class="marker--hr">'+
+              '<a href="#" class="marker--link">Show more</a>'
+      },
+      {
+        coordinates: {lat: 44.109401185859454, lng: -79.12068351989235},
+        image: '../img/farm-2.png',
+        info: '<div class="marker--title">"Uxbridge" FARM </div>'+'<hr class="marker--hr">'+
+              '<div class="marker--product">Product: Scottish Highland beef</div>'+'<hr class="marker--hr">'+
+              '<div class="marker--address">Address: Uxbridge, ON L9P1R1</div>'+'<hr class="marker--hr">'+
+              '<a href="#" class="marker--link">Show more</a>'
+      },
+      {
+        coordinates: {lat: 44.89910369903265, lng: -76.28509045763863},
+        image: '../img/farm-2.png',
+        info: '<div class="marker--title">"Perth" FARM</div>'+'<hr class="marker--hr">'+
+              '<div class="marker--product">Product: chicken</div>'+'<hr class="marker--hr">'+
+              '<div class="marker--address">Address: Perth, ON K7H3C9</div>'+'<hr class="marker--hr">'+
+              '<a href="#" class="marker--link">Show more</a>'
+      }
+    ]
     
-    addMarker({
-      coordinates: pos,
-      image: '../img/map-marker.png',
-      info: '<div>You are here!</div>'
-    });
-
+    for (var i = 0; i<markers.length; i++){
+      addMarker(markers[i]);
+    }
     function addMarker(properties){
       var marker = new google.maps.Marker({
         position: properties.coordinates,
@@ -42,7 +86,13 @@ function initMap() {
         });
       };
     };
-
+    // function handler(event){
+    //   var buttonn = event.relatedTarget;
+    //   console.log(buttonn);
+    // }
+    // document.getElementById('mapModal').addEventListener('show.bs.modal', function(e){
+    //   console.log(e)
+    // }, false)
   //   var app = Array.prototype;
   //   app.forEach.call(document.querySelectorAll('#mapModal'), function(el){
   //     el.addEventListener('show.bs.modal', function(){
